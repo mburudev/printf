e0f43193a74a1a2b8296ad0a25b21d7dd4401584
mburudev <ianmburu999@gmail.com>
2023-04-27 23:24:33 -0700

    Task 0

---
+++ task0.c
@@ -0,0 +1,27 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _printf - produces output according to a string format.
+ * @format: a character string
+ * Return: 0
+ */
+
+int _printf(const char *format, ...)
+{
+	int count = 0;
+	va_list params;
+
+	va_start(params, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			putchar('%');
+			count++;
+		}
+	}
+	return (count);
+}
