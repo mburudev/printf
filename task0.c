312b612a77359ea366d6ce06f396a72fff09e83d
mburudev <ianmburu999@gmail.com>
2023-05-03 05:15:41 -0700

    Task 1 betty corrections

--- task0.c
+++ task0.c
@@ -7,35 +7,36 @@
  * @format: a character string
  * Return: 0
  */
-int _printf(const char *format, ...) {
+int _printf(const char *format, ...)
+{
 	char  ch;
 	int count;
-    va_list args;
-    va_start(args, format);
+	va_list args;
 
-     count = 0;
-   
-    
+	va_start(args, format);
+	count = 0;
 
-    while ((ch = *format++) != '\0') {
-        if (ch == '%') {
-            ch = *format++;
-            switch (ch) {
-                case 'd':
-                case 'i':
-                    count += printf("%d", va_arg(args, int));
-                    break;
-                default:
-                    count += printf("%%%c", ch);
-                    break;
-            }
-        } else {
-            putchar(ch);
-            count++;
-        }
-    }
-
-    va_end(args);
-
-    return count;
+	while ((ch = *format++) != '\0')
+	{
+		if (ch == '%')
+		{
+			ch = *format++;
+			switch (ch)
+			{
+				case 'd':
+				case 'i':
+					count += printf("%d", va_arg(args, int));
+					break;
+				default:
+					count += printf("%%%c", ch);
+					break;
+			}
+		} else
+		{
+			putchar(ch);
+			count++;
+		}
+	}
+	va_end(args);
+	return (count);
 }
