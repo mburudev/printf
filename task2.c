d031a259785de84f4a9163ecc13b8082b4a3cc28
mburudev <ianmburu99@gmail.com>
2023-05-23 05:45:58 -0700

    Adhered to betty

--- task2.c
+++ task2.c
@@ -7,31 +7,35 @@
  * Return: unsigned int in binary.
  */
 
-int _printf(const char *format, ...) {
-  va_list args;
-  int count = 0;
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
 
-  va_start(args, format);
-  while (*format != '\0') {
-    if (*format == '%') {
-      switch (*++format) {
-        case 'c':
-          count += printf("%c", va_arg(args, int));
-          break;
-        case 's':
-          count += printf("%s", va_arg(args, char*));
-          break;
-        default:
-          count += printf("%%%c", *format);
-          break;
-      }
-    } else {
-      count += printf("%c", *format);
-    }
-    format++;
-  }
+	va_start(args, format);
 
-  va_end(args);
-
-  return count;
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			switch (*++format)
+			{
+			case 'c':
+				count += printf("%c", va_arg(args, int));
+				break;
+			case 's':
+				count += printf("%s", va_arg(args, char*));
+				break;
+			default:
+				count += printf("%%%c", *format);
+				break;
+			}
+		} else
+		{
+			count += printf("%c", *format);
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
 }
