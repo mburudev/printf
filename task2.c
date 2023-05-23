524f885268a6d5bd247b34775bb66ba911582cf8
mburudev <ianmburu99@gmail.com>
2023-05-23 07:05:09 -0700

    Task 3 adhered to betty

--- task2.c
+++ task2.c
@@ -6,37 +6,40 @@
  * @format: a character string
  * Return: unsigned int in binary.
  */
-int _printf(const char *format, ...) {
-  va_list args;
-  int count = 0;
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
 
-  va_start(args, format);
-  while (*format) {
-    if (*format == '%') {
-      switch (*++format) {
-        case 'u':
-          count += printf("%u", va_arg(args, unsigned int));
-          break;
-        case 'o':
-          count += printf("%o", va_arg(args, unsigned int));
-          break;
-        case 'x':
-          count += printf("%x", va_arg(args, unsigned int));
-          break;
-        case 'X':
-          count += printf("%X", va_arg(args, unsigned int));
-          break;
-        default:
-          count += putchar(*format);
-          break;
-      }
-    } else {
-      count += putchar(*format);
-    }
-    format++;
-  }
-
-  va_end(args);
-
-  return count;
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			switch (*++format)
+			{
+			case 'u':
+				count += printf("%u", va_arg(args, unsigned int));
+				break;
+			case 'o':
+				count += printf("%o", va_arg(args, unsigned int));
+				break;
+			case 'x':
+				count += printf("%x", va_arg(args, unsigned int));
+				break;
+			case 'X':
+				count += printf("%X", va_arg(args, unsigned int));
+				break;
+			default:
+				count += putchar(*format);
+				break;
+			}
+		} else
+		{
+			count += putchar(*format);
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
 }
